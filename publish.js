8d8106abd6171fa1f4134ceaa0c49f84b79b7b30
Tanner Nielsen <tannerntannern@gmail.com>
2020-12-26 01:11:58 -0600

    Add logging to publish script

--- publish.js
+++ publish.js
@@ -1,8 +1,10 @@
 const ghPages = require('gh-pages');
 
+console.info('Starting publish...');
 ghPages.publish('public', {
 	branch: 'gh-pages',
 	repo: 'https://github.com/tannerntannern/blog.git',
 }, err => {
 	console.error(err);
 });
+console.info('Publish complete!');
