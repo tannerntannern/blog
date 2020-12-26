092c059d5272a38b8afd1afa14e3f98fa29d46c3
Tanner Nielsen <tannerntannern@gmail.com>
2020-12-26 01:32:39 -0600

    Updates

---
+++ component---src-pages-404-tsx-ed026bcba210a242a782.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{"i6+/":function(n,t,s){"use strict";s.r(t);s("q1tI");var e=s("dH/O"),o=s("noox"),u=s("WsIH"),a=s("gZs9"),c=s("qKvR");t.default=function(){return Object(c.jsx)(e.a,null,Object(c.jsx)(u.a,null),Object(c.jsx)(o.a,null,Object(c.jsx)("div",{style:{marginTop:"100px"}},Object(c.jsx)(a.a.h1,null,"404: Page Not Found"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ed026bcba210a242a782.js.map
